cac9e20f4e8af8f81c4d740708c79c124d0237e2
김홍석 <hongsuk2615@gmail.com>
2025-08-16 17:31:47 +0900

    HALO / Overwatch 프로젝트 상세내용

--- src/main/java/com/portfolio/portfolio/controller/IndexController.java
+++ src/main/java/com/portfolio/portfolio/controller/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public  String index(){
         return "index";
     }
+
+    @GetMapping("/overwatch")
+    public  String overwatch(){
+        return "project/overwatch";
+    }
 }
